4f5ce6c6c45a8582d7eb1dc94dd8ce247ad41d6a
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-04-23 23:29:45 +0430

    simplify game.dart file

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,7 +1,35 @@
+
 import 'dart:ui';
 
 import 'package:flame/game.dart';
 
+//import 'package:trex/game/Horizon/horizon.dart';
+//import 'package:trex/game/game_config.dart';
+//import 'package:trex/game/t_rex/t_rex.dart';
+
+enum TRexGameStatus { playing, waiting, gameOver }
+
 class TRexGame extends BaseGame {
-  TRexGame({Image spriteImage});
+//  TRex tRex;
+//  Horizon horizon;
+  TRexGameStatus status = TRexGameStatus.waiting;
+
+  TRexGame({Image spriteImage}) {
+//    tRex = new TRex(spriteImage);
+//    horizon = new Horizon(spriteImage);
+
+//    this..add(tRex)..add(horizon);
+  }
+
+  @override
+  void update(double t) {
+//    tRex.update(t);
+//    horizon.update(t);
+  }
+
+  void onTap() {
+//    tRex.startJump(GameConfig.speed);
+  }
+
+  bool get playing => status == TRexGameStatus.playing;
 }
